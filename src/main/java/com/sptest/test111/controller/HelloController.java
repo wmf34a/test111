8f7372c53353da2410a7c9fe7aed77f0af8a025f
Yeonseung Choi <wmf34a@naver.com>
2021-06-16 18:15:43 +0900

    sprint init

---
+++ src/main/java/com/sptest/test111/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sptest.test111.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hi HI soo";
+    }
+
+}
---
+++ src/test/java/com/sptest/test111/Test111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sptest.test111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
